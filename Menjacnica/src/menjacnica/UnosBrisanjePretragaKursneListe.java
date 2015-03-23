6ef395d9946391941c89d98752c83f7b17fc1234
randjelovicaleksandar <randjelovic8913@fon.bg.ac.rs>
2015-03-23 22:15:23 +0100

    Napravljena nova klasa UnosBrisanjePretragaKursneListe koja implementira metode interfejsa MenjacnicaInterfejs. Za sada ove metode ostaju sa praznim telom.

---
+++ Menjacnica/src/menjacnica/UnosBrisanjePretragaKursneListe.java
@@ -0,0 +1,22 @@
+package menjacnica;
+
+import menjacnica.interfejs.MenjacnicaInterfejs;
+
+public class UnosBrisanjePretragaKursneListe implements MenjacnicaInterfejs {
+
+	public void dodajKurs() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void obrisiKurs() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public String pronadjiKurs() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}

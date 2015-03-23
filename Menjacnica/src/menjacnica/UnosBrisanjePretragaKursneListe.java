07fb1831851a70a9941df46c76cb3342f5f1d416
randjelovicaleksandar <randjelovic8913@fon.bg.ac.rs>
2015-03-23 22:48:59 +0100

    Klasa UnosPretragaBrisanjeKursneListe koja implementira interfejs MenjacnicaInterfejs je potpuno implementirana. Takodje, u ovom interfejsu su ubaceni potrebni ulazni parametri za sve metode.

--- Menjacnica/src/menjacnica/UnosBrisanjePretragaKursneListe.java
+++ Menjacnica/src/menjacnica/UnosBrisanjePretragaKursneListe.java
@@ -1,22 +1,46 @@
 package menjacnica;
 
+import java.util.GregorianCalendar;
+
 import menjacnica.interfejs.MenjacnicaInterfejs;
 
-public class UnosBrisanjePretragaKursneListe implements MenjacnicaInterfejs {
 
-	public void dodajKurs() {
-		// TODO Auto-generated method stub
-		
+public class UnosBrisanjePretragaKursneListe extends Menjacnica implements MenjacnicaInterfejs {
+
+	public void dodajKurs(String nazivValute, String skracenica, GregorianCalendar datum, 
+			double kupovniKurs, double prodajniKurs, double srednjiKurs) {
+		Valuta valuta = new Valuta();
+		valuta.setNazivValute(nazivValute);
+		valuta.setSkracenica(skracenica);
+		valuta.setDatum(datum);
+		valuta.setKupovniKurs(kupovniKurs);
+		valuta.setProdajniKurs(prodajniKurs);
+		valuta.setSrednjiKurs(srednjiKurs);
+		kursnaLista.add(valuta);
 	}
 
-	public void obrisiKurs() {
-		// TODO Auto-generated method stub
-		
+	public void obrisiKurs(String valuta, GregorianCalendar datum) {
+		boolean obrisan = false;
+		for (int i = 0; i < kursnaLista.size(); i++) {
+			if(kursnaLista.get(i).getNazivValute().equals(valuta) && 
+					kursnaLista.get(i).getDatum().equals(datum)) {
+				kursnaLista.remove(kursnaLista.get(i));
+				obrisan = true;
+			}
+		}
+		if(!obrisan) {
+			System.out.println("Nije pronadjen kurs valute: " +valuta+ " na datum koji ste uneli");
+		}
 	}
 
-	public String pronadjiKurs() {
-		// TODO Auto-generated method stub
-		return null;
+	public String pronadjiKurs(String valuta, GregorianCalendar datum) {
+		for (int i = 0; i < kursnaLista.size(); i++) {
+			if(kursnaLista.get(i).getNazivValute().equals(valuta) && 
+					kursnaLista.get(i).getDatum().equals(datum)) {
+				return kursnaLista.get(i).toString();
+			}
+		}
+		return "Nije pronadjen kurs koji ste zeleli";
 	}
 
 }
--- Menjacnica/src/menjacnica/interfejs/MenjacnicaInterfejs.java
+++ Menjacnica/src/menjacnica/interfejs/MenjacnicaInterfejs.java
@@ -1,11 +1,14 @@
 package menjacnica.interfejs;
 
+import java.util.GregorianCalendar;
+
 public interface MenjacnicaInterfejs {
 
-	public void dodajKurs();
+	public void dodajKurs(String nazivValute, String skracenica, GregorianCalendar datum, 
+			double kupovniKurs, double prodajniKurs, double srednjiKurs);
 	
-	public void obrisiKurs();
+	public void obrisiKurs(String valuta, GregorianCalendar datum);
 	
-	public String pronadjiKurs();
+	public String pronadjiKurs(String valuta, GregorianCalendar datum);
 		
 }
